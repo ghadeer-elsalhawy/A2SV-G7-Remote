4b533abaa2a2d9d866279b47463732d657b9cc44
Ghadeer Elsalhawy <ghadeer.elsalhawy@gmail.com>
2026-01-28 11:40:19 +0200

    Create power-mod-power.py

---
+++ 1_Python_Basics/power-mod-power.py
@@ -0,0 +1,7 @@
+# Problem Link: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+a = int(input())
+b = int(input())
+m = int(input())
+print(a ** b)
+print(pow(a, b, m))
