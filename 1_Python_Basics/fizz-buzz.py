ee1ce87fbe663d308cfcc2c4fbe8397f44d466a6
Ghadeer Elsalhawy <ghadeer.elsalhawy@gmail.com>
2026-02-02 20:11:42 +0200

    Add fizz-buzz.py

---
+++ 1_Python_Basics/fizz-buzz.py
@@ -0,0 +1,15 @@
+# Problem link: https://leetcode.com/problems/fizz-buzz/description/
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = [""] * n
+        for i in range(n):
+            if (i + 1) % 3 == 0 and (i + 1) % 5 == 0:
+                res[i] = "FizzBuzz"
+            elif (i + 1) % 3 == 0:
+                res[i] = "Fizz"
+            elif (i + 1) % 5 == 0:
+                res[i] = "Buzz"
+            else:
+                res[i] = str(i + 1)
+        return res
