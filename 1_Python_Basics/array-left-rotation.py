607fb1fd1274f82e21b61ec171d1ce3bb345c7f8
Ghadeer Elsalhawy <ghadeer.elsalhawy@gmail.com>
2026-02-06 21:03:06 +0200

    Adding Python basics problems solutions

---
+++ 1_Python_Basics/array-left-rotation.py
@@ -0,0 +1,24 @@
+# Problem link: https://www.hackerrank.com/challenges/array-left-rotation/problem
+
+import os 
+
+def rotateLeft(d, arr):
+    # Write your code here
+    return arr[d:] + arr[:d]
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ 1_Python_Basics/concatenation-of-array.py
@@ -0,0 +1,5 @@
+# Problem link: https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
