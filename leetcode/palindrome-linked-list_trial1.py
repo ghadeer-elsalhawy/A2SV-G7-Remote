a046c29bc9145a14555b6109e7762163755a52c6
Ghadeer Elsalhawy <ghadeer.elsalhawy@gmail.com>
2026-04-04 19:17:41 +0200

    Add solution for Palindrome Linked List (trial 1)

---
+++ leetcode/palindrome-linked-list_trial1.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        s = ""
+        while head:
+            s += str(head.val)
+            head = head.next
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
